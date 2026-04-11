20a335d294ff00f947131b2436eec71d978d94e6
Kinyi <kinyi9461@gmail.com>
2026-04-12 00:10:57 +0300

    all

--- shared-logistics-backend/utils/setTokenCookie.js
+++ shared-logistics-backend/utils/setTokenCookie.js
@@ -3,7 +3,7 @@ const setTokenCookie = (res, token) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // true in prod
-    sameSite: "Strict",
+    sameSite: "None",
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
   });
 };
